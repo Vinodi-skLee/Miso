a3d79b75a1e77142c413b208f518eeaf09ce84f1
Dazunx <22100493.handong.ac.kr>
2022-07-31 00:39:56 +0900

    홈 화면 bottomAppbar 색깔 수정, 아이콘과 페이지 연결

--- lib/home.dart
+++ lib/home.dart
@@ -1,10 +1,10 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:miso/LoginPage.dart';
 import 'package:get/get.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:miso/chat.dart';
 
+import 'package:miso/chat.dart';
+import 'package:page_transition/page_transition.dart';
 import 'notification.dart';
 import 'hamberger.dart';
 import 'Menu/appGuide.dart';
@@ -26,6 +26,18 @@ class _MisoHomeState extends State<MisoHome> {
         title: Text('MISO'),
         backgroundColor: Color(0xff54AA8B),
         centerTitle: true,
+        leading:
+          IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: () {
+              Navigator.push(context,
+                  PageTransition(
+                    type: PageTransitionType.leftToRight,
+                    child: Hamberger(),
+                  )
+              );
+            },
+          ),
         actions: [
           IconButton(
             icon: Icon(Icons.notifications_none),
@@ -35,7 +47,8 @@ class _MisoHomeState extends State<MisoHome> {
           ),
         ],
       ),
-      drawer: Hamberger(),
+
+
       body: Container(
           child: Column(
             children: [
@@ -44,9 +57,11 @@ class _MisoHomeState extends State<MisoHome> {
                 size: 300,
               ),
             ],
-          )),
+          )
+      ),
 
       bottomNavigationBar: BottomAppBar(
+        color: Color(0xff54AA8B),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
@@ -56,7 +71,7 @@ class _MisoHomeState extends State<MisoHome> {
                   Get.to(MisoHome());
                 },
                 icon: Icon(Icons.home,
-                  color: Color(0xff54AA8B),)
+                  color: Colors.white,)
             ),
 
             Spacer(),
@@ -65,8 +80,8 @@ class _MisoHomeState extends State<MisoHome> {
                 onPressed: (){
                   Get.to(Chat());
                 },
-                icon: Icon(Icons.chat,
-                  color: Color(0xff54AA8B),)
+                icon: Icon(Icons.chat_outlined,
+                  color: Colors.white,)
             ),
 
             Spacer(),
@@ -75,8 +90,8 @@ class _MisoHomeState extends State<MisoHome> {
                 onPressed: (){
                   Get.to(Login());
                 },
-                icon: Icon(Icons.account_circle,
-                  color: Color(0xff54AA8B),)
+                icon: Icon(Icons.account_circle_outlined,
+                  color: Colors.white,)
             ),
             SizedBox(width: 30,)
           ],
